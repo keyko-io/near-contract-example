c4dd6f4aefe0f973ee059bf7d18f421c6a0dcdb2
Thiago Machado <thiago@keyko.io>
2022-06-20 09:00:54 -0300

    feat(counter): binary example init

--- contracts/contract-01/src/main.rs
+++ contracts/contract-01/src/main.rs
@@ -1,3 +1,37 @@
+pub struct Counter {
+    pub value: u32,
+}
+
+impl Counter {
+    /// Creates a new instance. `value` starts at zero.
+    pub fn new() -> Self {
+        Counter { value: 0 }
+    }
+
+    pub fn increment(&mut self) {
+        self.value += 1;
+    }
+
+    pub fn decrement(&mut self) {
+        self.value -= 1;
+    }
+
+    pub fn set(&mut self, new_value: u32) {
+        self.value = new_value;
+    }
+
+    pub fn print_value(&self) {
+        println!("{}", self.value);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut counter = Counter::new();
+    counter.print_value();
+    counter.increment();
+    counter.print_value();
+    counter.decrement();
+    counter.print_value();
+    counter.set(2);
+    counter.print_value();
 }
